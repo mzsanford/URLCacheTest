ad2bcb25ae274add128759231041dcf7904e51d0
Matt Sanford <matt@mzsanford.com>
2013-11-14 15:39:58 -0800

    Add Authorization header test as well

--- URLCacheTest/MZSViewController.m
+++ URLCacheTest/MZSViewController.m
@@ -16,6 +16,7 @@
 @property (nonatomic, weak) IBOutlet UITextField *acceptHeader;
 @property (nonatomic, weak) IBOutlet UISwitch *clearCacheSwitch;
 @property (nonatomic, weak) IBOutlet UISwitch *varyUrlSwitch;
+@property (nonatomic, strong) UITapGestureRecognizer *tap;
 
 @end
 
@@ -26,12 +27,21 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self updateCacheSize];
+
+    self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                       action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:self.tap];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dismissKeyboard
+{
+    [self.acceptHeader resignFirstResponder];
 }
 
 - (void)updateCacheSize
@@ -43,13 +53,13 @@
     self.currentSizeLabel.text = [@"Current Size: \n\t" stringByAppendingString:values];
 }
 
-- (IBAction)tappedMakeRequest:(id)sender
+- (NSMutableURLRequest *)createURLRequest
 {
+    // No vary header
     NSURL *url = [NSURL URLWithString:@"https://abs.twimg.com/errors/twitter_web_sprite_icons.png"];
+    // Vary: *
+    // NSURL *url = [NSURL URLWithString:@"http://stackoverflow.com/questions/1975416/trying-to-understand-the-vary-http-header"];
     NSString *acceptHeaderValue = self.acceptHeader.text;
-    if (self.clearCacheSwitch.on) {
-        [[NSURLCache sharedURLCache] removeAllCachedResponses];
-    }
 
     if (self.varyUrlSwitch.on) {
         url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?accept=%@",
@@ -62,17 +72,49 @@
                                                               cachePolicy:NSURLCacheStorageAllowed
                                                           timeoutInterval:2.0];
 
-    NSLog(@"Requesting URL: %@", [url absoluteString]);
     if (acceptHeaderValue && ![acceptHeaderValue isEqualToString:@""]) {
         [urlRequest setValue:acceptHeaderValue forHTTPHeaderField:@"Accept"];
-        NSLog(@"Sending accept header value: %@", acceptHeaderValue);
     }
 
-    NSCachedURLResponse *cachedResponse = [[NSURLCache sharedURLCache] cachedResponseForRequest:urlRequest];
+    return urlRequest;
+}
+
+- (IBAction)tappedMakeRequest:(id)sender
+{
+    NSURLRequest *urlRequest = [self createURLRequest];
+    [self makeTimedRequest:urlRequest];
+}
+
+- (IBAction)makeAuthenticatedRequest:(id)sender
+{
+    NSMutableURLRequest *urlRequest = [self createURLRequest];
+
+    // Add auth header
+    NSString *username = [self.class randomAlphanumericStringWithLength:10];
+    NSString *password = [self.class randomAlphanumericStringWithLength:10];
+    NSString *encoded =  [[NSString alloc] initWithData:[[[NSString stringWithFormat:@"%@:%@", username, password] dataUsingEncoding:NSUTF8StringEncoding]
+                         base64EncodedDataWithOptions:0] encoding:NSUTF8StringEncoding];
+
+    NSString *authString = [NSString stringWithFormat:@"Basic %@", encoded];
+
+    [urlRequest setValue:authString forHTTPHeaderField:@"Authorization"];
+
+    [self makeTimedRequest:urlRequest];
+}
+
+- (void)makeTimedRequest:(NSURLRequest *)request
+{
+    if (self.clearCacheSwitch.on) {
+        [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    }
+
+    NSCachedURLResponse *cachedResponse = [[NSURLCache sharedURLCache] cachedResponseForRequest:request];
     BOOL servedFromCache = (cachedResponse != nil);
 
     NSURLResponse *response = nil;
     NSError *error = nil;
+
+    NSLog(@"Headers: %@", [request allHTTPHeaderFields] );
 
     mach_timebase_info_data_t info;
     if (mach_timebase_info(&info) != KERN_SUCCESS) {
@@ -81,9 +123,9 @@
     }
 
     uint64_t start = mach_absolute_time();
-    NSData *data = [NSURLConnection sendSynchronousRequest:urlRequest
-                                          returningResponse:&response
-                                                      error:&error];
+    NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                         returningResponse:&response
+                                                     error:&error];
     uint64_t end = mach_absolute_time();
     uint64_t elapsed = end - start;
 
@@ -94,8 +136,22 @@
           (error == nil ? @"Non-error" : @"Error"),
           [data length],
           timeInSecs);
-    
+
     [self updateCacheSize];
 }
+
++ (NSString *)randomAlphanumericStringWithLength:(NSInteger)length
+{
+    NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    NSMutableString *randomString = [NSMutableString stringWithCapacity:length];
+
+    for (int i = 0; i < length; i++) {
+        [randomString appendFormat:@"%C", [letters characterAtIndex:arc4random() % [letters length]]];
+    }
+
+    return randomString;
+}
+
+
 
 @end
